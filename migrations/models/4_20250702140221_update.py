b3029ee9cebec2cbb6100c70f1346e2edb82aac4
Viktoria <v.s.polikanova@gmail.com>
2025-07-02 14:34:39 +0700

    миграция

--- migrations/models/4_20250702140221_update.py
+++ migrations/models/4_20250702140221_update.py
@@ -3,7 +3,7 @@ from tortoise import BaseDBAsyncClient
 
 async def upgrade(db: BaseDBAsyncClient) -> str:
     return """
-        ALTER TABLE "legal_entities" DROP CONSTRAINT "fk_legal_en_legal_en_f479f5ea";
+
         ALTER TABLE "legal_entities" DROP COLUMN "entity_type_id";
         DROP TABLE IF EXISTS "legal_entity_types";
         DROP TABLE IF EXISTS "entity_company_relations";"""
@@ -12,4 +12,4 @@ async def upgrade(db: BaseDBAsyncClient) -> str:
 async def downgrade(db: BaseDBAsyncClient) -> str:
     return """
         ALTER TABLE "legal_entities" ADD "entity_type_id" VARCHAR(255);
-        ALTER TABLE "legal_entities" ADD CONSTRAINT "fk_legal_en_legal_en_f479f5ea" FOREIGN KEY ("entity_type_id") REFERENCES "legal_entity_types" ("id") ON DELETE CASCADE;"""
+        ALTER TABLE "legal_entities" ADD CONSTRAINT "fk_legal_entity" FOREIGN KEY ("entity_type_id") REFERENCES "legal_entity_types" ("id") ON DELETE CASCADE;"""
