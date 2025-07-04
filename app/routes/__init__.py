a71907890c6f59fa253e022b8ebca440d7c116fe
Viktoria <v.s.polikanova@gmail.com>
2025-07-04 11:15:59 +0700

    выпилила аутентификацию регистрацию и приглашения

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,8 +1,5 @@
 from fastapi import FastAPI
-from tiacore_lib.routes.auth_route import auth_router
 from tiacore_lib.routes.company_route import company_router
-from tiacore_lib.routes.invite_route import invite_router
-from tiacore_lib.routes.register_route import register_router
 from tiacore_lib.routes.user_route import user_router
 
 from .cash_register_route import cash_register_router
@@ -13,9 +10,6 @@ from .warehouse_route import warehouse_router
 
 
 def register_routes(app: FastAPI):
-    app.include_router(auth_router, prefix="/auth", tags=["Auth"])
-    app.include_router(invite_router, prefix="/api", tags=["Invite"])
-    app.include_router(register_router, prefix="/api", tags=["Register"])
     app.include_router(user_router, prefix="/api/users", tags=["Users"])
     app.include_router(company_router, prefix="/api/companies", tags=["Companies"])
     app.include_router(
